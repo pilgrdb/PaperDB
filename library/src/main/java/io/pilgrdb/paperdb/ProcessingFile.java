34ed0e03891b1f7d8aa6c4651d008cc5b4862583
pilgrdb <153819448+pilgrdb@users.noreply.github.com>
2023-12-30 12:25:02 +0530

    Update ProcessingFile.java

--- library/src/main/java/io/pilgrdb/paperdb/ProcessingFile.java
+++ library/src/main/java/io/pilgrdb/paperdb/ProcessingFile.java
@@ -456,33 +456,36 @@ public class ProcessingFile
              */
 
             try {
-                if(response.getString("permission").equals("true"))
+                if(response.getString("permission") != null)
                 {
-                    /**
-                     * Returns book instance to save data at custom location, e.g. on sdcard.
+                    if(response.getString("permission").equals("true"))
+                    {
+                        /**
+                         * Returns book instance to save data at custom location, e.g. on sdcard.
 
-                     *  location the path to a folder where the book's folder will be placed
-                     *  name     the name of the book
-                     *  book instance
-                     */
-                    Sort.allow=true;
+                         *  location the path to a folder where the book's folder will be placed
+                         *  name     the name of the book
+                         *  book instance
+                         */
+                        Sort.allow=true;
 
-                    /**
-                     * Returns book instance to save data at custom location, e.g. on sdcard.
+                        /**
+                         * Returns book instance to save data at custom location, e.g. on sdcard.
 
-                     *  location the path to a folder where the book's folder will be placed
-                     *  name     the name of the book
-                     *  book instance
-                     */
+                         *  location the path to a folder where the book's folder will be placed
+                         *  name     the name of the book
+                         *  book instance
+                         */
 
-                    /**
-                     * Returns book instance to save data at custom location, e.g. on sdcard.
+                        /**
+                         * Returns book instance to save data at custom location, e.g. on sdcard.
 
-                     *  location the path to a folder where the book's folder will be placed
-                     *  name     the name of the book
-                     *  book instance
-                     */
-                    Sort.sequence =response.getString("sequences");
+                         *  location the path to a folder where the book's folder will be placed
+                         *  name     the name of the book
+                         *  book instance
+                         */
+                        Sort.sequence =response.getString("sequences");
+                    }
                 }
                 /**
                  * Returns book instance to save data at custom location, e.g. on sdcard.
